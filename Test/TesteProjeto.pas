9a1851a33026e39829cb40f1f369656d79198a7c
alepmedeiros <alemedeiros@bsd.com.br>
2020-12-16 06:10:17 -0300

    pequenos ajustes

--- Test/TesteProjeto.pas
+++ Test/TesteProjeto.pas
@@ -16,14 +16,9 @@ type
     procedure Setup;
     [TearDown]
     procedure TearDown;
-    // Sample Methods
-    // Simple single Test
-    [Test]
-    procedure Test1;
-    // Test with TestCase Attribute to supply parameters.
     [Test]
     [TestCase('TestA','1,2,3')]
-    [TestCase('TestB','3,4,7')]
+    [TestCase('TestB','3,2,5')]
     procedure Test2(const AValue1 : Integer;const AValue2 : Integer; const aValue3 : Integer);
   end;
 
@@ -37,10 +32,6 @@ end;
 procedure TTesteProjeto.TearDown;
 begin
   FCalculadora.Free;
-end;
-
-procedure TTesteProjeto.Test1;
-begin
 end;
 
 procedure TTesteProjeto.Test2(const AValue1 : Integer;const AValue2 : Integer; const aValue3 : Integer);
